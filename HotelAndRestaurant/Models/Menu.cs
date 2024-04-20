67613dd5739896275c28779065771a6211719e6b
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-04-21 00:19:32 +0200

    Krijimi i klases 'Menu'

---
+++ HotelAndRestaurant/Models/Menu.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace HotelAndRestaurant.Models
+{
+    public class Menu
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FoodID { get; set; }
+    }
+}
