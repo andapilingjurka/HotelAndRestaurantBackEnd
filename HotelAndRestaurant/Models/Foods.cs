1026b65d6ab724ec4e74ac03e53117313c32cd8a
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-04-21 00:18:25 +0200

    Krijimi i klases 'Foods'

---
+++ HotelAndRestaurant/Models/Foods.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace HotelAndRestaurant.Models
+{
+    public class Foods
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+    }
+
+
+}
