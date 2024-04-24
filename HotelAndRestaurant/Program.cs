40b34b8d77afbde338c7ecbf5869a5c4f9ebceb4
anda <ap56170@ubt-uni.net>
2024-04-23 21:19:31 -0700

    Rrregullimi i ConnectionString ne Program.cs

--- HotelAndRestaurant/Program.cs
+++ HotelAndRestaurant/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
 using Swashbuckle.AspNetCore.Swagger;
 
@@ -7,18 +8,34 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Add Swagger
+builder.Services.AddSwaggerGen();
+
+
 // MongoDB connection string
 var mongoConnectionString = "mongodb://localhost:27017";
 builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoConnectionString));
 
-// Add Swagger
-builder.Services.AddSwaggerGen();
+//ConnectionString
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDBConnection"));
+    }
+    );
+
 
 var app = builder.Build();
+
+
 app.UseCors(policy => policy.AllowAnyHeader()
-    .AllowAnyMethod()
-    .SetIsOriginAllowed(origin => true)
-    .AllowCredentials());
+                               .AllowAnyMethod()
+                               .SetIsOriginAllowed(origin => true)
+                               .AllowCredentials()
+
+                               );
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
