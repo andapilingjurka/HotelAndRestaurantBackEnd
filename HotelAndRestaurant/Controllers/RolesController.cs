1b8c18b30a9bee76adb80c01aabc022211ff8133
ArdiBerdyna <ardi_881@hotmail.com>
2024-04-24 15:43:38 +0200

    implementimi i RoleController

---
+++ HotelAndRestaurant/Controllers/RolesController.cs
@@ -0,0 +1,73 @@
+﻿using HotelAndRestaurant.Data;
+using HotelAndRestaurant.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelAndRestaurant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public RolesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //ListAll
+        [HttpGet]
+        [Route("GetAllList")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var role = await _db.Roles.ToListAsync();
+            return Ok(role);
+        }
+
+        //GetById
+        [HttpGet]
+        [Route("GetRoleById")]
+        public async Task<IActionResult> GetRoleById(int Id)
+        {
+            var role = await _db.Roles.FindAsync(Id);
+            return Ok(role);
+        }
+
+        //Add
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> PostAsync(Role role)
+        {
+            _db.Roles.Add(role);
+            await _db.SaveChangesAsync();
+            return Created($"/GetRoleById/{role.Id}", role);
+        }
+
+        //Update
+        [HttpPut]
+        [Route("Update/{id}")]
+        public async Task<IActionResult> PutAsync(Role role)
+        {
+            _db.Roles.Update(role);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+
+        //Delete
+        [Route("Delete")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(int Id)
+        {
+            var roleDelete = await _db.Roles.FindAsync(Id);
+            if (roleDelete == null)
+            {
+                return NotFound();
+            }
+            _db.Roles.Remove(roleDelete);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
