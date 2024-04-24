cf3c587b799108618f9c5d64bb2bdc76e75c0057
Erion Berdynaj <erion.berdyna@hotmail.com>
2024-04-24 20:27:38 +0200

    Implementimi i Staf dhe RewardBonus Controllerit

---
+++ HotelAndRestaurant/Controllers/RewardBonusController.cs
@@ -0,0 +1,81 @@
+﻿using HotelAndRestaurant.Data;
+using HotelAndRestaurant.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelAndRestaurant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RewardBonusController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public RewardBonusController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // ListAll
+        [HttpGet]
+        [Route("GetAllList")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var rewardBonuses = await _db.rewardBonus.ToListAsync();
+            return Ok(rewardBonuses);
+        }
+
+        // GetById
+        [HttpGet]
+        [Route("GetRewardBonusById")]
+        public async Task<IActionResult> GetRewardBonusByIdAsync(int id)
+        {
+            var rewardBonus = await _db.rewardBonus.FindAsync(id);
+            return Ok(rewardBonus);
+        }
+
+        // Add
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> PostAsync(RewardBonus rewardBonus)
+        {
+            _db.rewardBonus.Add(rewardBonus);
+            await _db.SaveChangesAsync();
+            return Created($"/GetRewardBonusById/{rewardBonus.Id}", rewardBonus);
+        }
+
+        // Update
+        [HttpPut]
+        [Route("Update/{id}")]
+        public async Task<IActionResult> PutAsync(int id, RewardBonus rewardBonus)
+        {
+            if (id != rewardBonus.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(rewardBonus).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // Delete
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var rewardBonusToDelete = await _db.rewardBonus.FindAsync(id);
+
+            if (rewardBonusToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _db.rewardBonus.Remove(rewardBonusToDelete);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ HotelAndRestaurant/Controllers/StaffController.cs
@@ -0,0 +1,83 @@
+﻿using HotelAndRestaurant.Data;
+using HotelAndRestaurant.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelAndRestaurant.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StaffController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // ListAll
+        [HttpGet]
+        [Route("GetAllList")]
+        public async Task<IActionResult> GetAsync()
+        {
+            var staffList = await _db.Stafi.ToListAsync();
+            return Ok(staffList);
+        }
+
+        // GetById
+        [HttpGet]
+        [Route("GetStaffById")]
+        public async Task<IActionResult> GetStaffByIdAsync(int stafiId)
+        {
+            var staff = await _db.Stafi.FirstOrDefaultAsync(s => s.stafiId == stafiId);
+            return Ok(staff);
+        }
+
+        // Add
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> PostAsync(stafi staff)
+        {
+            _db.Stafi.Add(staff);
+            await _db.SaveChangesAsync();
+            return Created($"/GetStaffById/{staff.stafiId}", staff);
+        }
+
+        // Update
+        [HttpPut]
+        [Route("Update/{stafiId}")]
+        public async Task<IActionResult> PutAsync(int stafiId, stafi staff)
+        {
+            if (stafiId != staff.stafiId)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(staff).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+
+        // Delete
+        [HttpDelete]
+        [Route("Delete/{stafiId}")]
+        public async Task<IActionResult> DeleteAsync(int stafiId)
+        {
+            var staffToDelete = await _db.Stafi.FindAsync(stafiId);
+
+            if (staffToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _db.Stafi.Remove(staffToDelete);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
