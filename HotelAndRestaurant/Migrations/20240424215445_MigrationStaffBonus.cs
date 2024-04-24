bf339f210d4ca2c311552ae65742b8ea5c2f7e15
Dea <dk61403@ubt-uni.net>
2024-04-24 23:59:12 +0200

    Migration for Staff and Bonus

---
+++ HotelAndRestaurant/Migrations/20240424215445_MigrationStaffBonus.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelAndRestaurant.Migrations
+{
+    public partial class MigrationStaffBonus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "rewardBonus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rewardBonus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stafi",
+                columns: table => new
+                {
+                    stafiId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    id = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    nrPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    cualification = table.Column<int>(type: "int", nullable: false),
+                    RewardBonusId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stafi", x => x.stafiId);
+                    table.ForeignKey(
+                        name: "FK_Stafi_rewardBonus_RewardBonusId",
+                        column: x => x.RewardBonusId,
+                        principalTable: "rewardBonus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stafi_RewardBonusId",
+                table: "Stafi",
+                column: "RewardBonusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stafi");
+
+            migrationBuilder.DropTable(
+                name: "rewardBonus");
+        }
+    }
+}
