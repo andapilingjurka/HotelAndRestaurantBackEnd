e1a3e6d3e08cc42ae7af614a513ccc65e9039b06
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-06-02 00:00:05 +0200

    Krijimi i klases 'Payment'

---
+++ HotelAndRestaurant/Models/Payment.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelAndRestaurant.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Amount { get; set; }
+        public int BookingID { get; set; }
+
+        public Booking Booking { get; set; }
+    }
+}
