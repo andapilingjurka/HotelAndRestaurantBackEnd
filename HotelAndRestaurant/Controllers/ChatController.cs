b26415814a689a08ce55a94ba085fe5335d64f31
Dea <dk61403@ubt-uni.net>
2024-06-05 12:31:00 +0200

    commit

---
+++ HotelAndRestaurant/Controllers/ChatController.cs
@@ -0,0 +1,37 @@
+﻿
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+
+    namespace HotelAndRestaurant.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ChatController : ControllerBase
+        {
+            private static readonly Dictionary<string, string> faqs = new Dictionary<string, string>
+        {
+            { "What is your name?", "My name is ChatBot." },
+            { "How are you?", "I'm just a bot, but I'm functioning properly!" },
+            { "What is Bambus Group?", "Bambus Group is a corporation founded in 2014, specializing in integrating the digital world into all business areas." },
+            { "Where is Bambus Group located?", "Bambus Group has offices in Kosovo, Albania, and Germany." }
+        };
+
+            [HttpPost]
+            public IActionResult Post([FromBody] QuestionModel questionModel)
+            {
+                var question = questionModel.Question.ToLower();
+                if (faqs.TryGetValue(question, out var answer))
+                {
+                    return Ok(new { answer });
+                }
+                return Ok(new { answer = "I don't know the answer to that question." });
+            }
+        }
+
+        public class QuestionModel
+        {
+            public string Question { get; set; }
+        }
+    }
+
+
---
+++ HotelAndRestaurant/Migrations/20240605095940_KontaktCrud.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelAndRestaurant.Migrations
+{
+    public partial class KontaktCrud : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RefreshTokenExpiryTime",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RefreshTokenExpiryTime",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "RefreshTokenExpiryTime",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "RefreshTokenExpiryTime",
+                value: null);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RefreshTokenExpiryTime",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RefreshTokenExpiryTime",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "RefreshTokenExpiryTime",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "RefreshTokenExpiryTime",
+                value: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
